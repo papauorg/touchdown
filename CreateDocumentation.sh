604c2c152f3aab6de6fc332b1ab7e2bdd6c05e49
Philipp <philipp.grathwohl@papau.org>
2012-11-21 22:23:21 +0100

    Delete unneeded xml

--- CreateDocumentation.sh
+++ CreateDocumentation.sh
@@ -2,6 +2,12 @@
 
 # Build solution using Releasemode
 
+
+#
+# Monodocer info here: http://www.mono-project.com/Generating_Documentation
+#
+
 # Use monodocer to convert XML-Comments to Monodoc format and merge them to the current documentation.
+mdoc --exceptions=added,asm -i src/Touchdown.Core/bin/Release/TouchdownCore.xml -o doc/MonoDoc
 
 # Create single monodoc file
